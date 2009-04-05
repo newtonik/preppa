6bad68d4c642478c601776dd416b4ed1a537fc19
newtonik <newtonik@gmail.com>
2009-04-05 14:51:18 -0400

    starting the template for css

---
+++ src/com/preppa/web/components/Layout.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.preppa.web.components;
+
+import org.apache.tapestry5.annotations.IncludeStylesheet;
+
+/**
+ *
+ * @author newtonik
+ */
+@IncludeStylesheet("context:styles/site.css")
+public class Layout {
+
+}
