a3317f6afdd30a2d84137040c03c71db7d998bd7
jsoliman <devnull@localhost>
2009-04-12 22:01:35 -0700

    updated with edit vocab. Still getting an exception however.

--- src/com/preppa/web/pages/contribution/vocab/NewVocab.java
+++ src/com/preppa/web/pages/contribution/vocab/NewVocab.java
@@ -6,7 +6,7 @@ package com.preppa.web.pages.contribution.vocab;
 
 import com.preppa.web.data.VocabDAO;
 import com.preppa.web.entities.Vocab;
-import com.preppa.web.pages.Index;
+import com.preppa.web.pages.contribution.vocab.ShowVocab;
 import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.hibernate.annotations.CommitAfter;
@@ -20,10 +20,10 @@ public class NewVocab {
 
     @Property
     private Vocab vocab;
-    @InjectPage
-    private Index index;
     @Inject
     private VocabDAO vocabDAO;
+    @InjectPage
+    private ShowVocab showvocab;
 
     void onActivate(Vocab word) {
         this.vocab = word;
@@ -39,6 +39,7 @@ public class NewVocab {
          vocab.setUpdatedAt(new java.sql.Timestamp(System.currentTimeMillis()));
 
          vocabDAO.doSave(vocab);
-        return index;
+         showvocab.setvocab(vocab);
+        return showvocab;
     }
 }
