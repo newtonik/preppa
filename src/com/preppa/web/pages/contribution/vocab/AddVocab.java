c1089d5d75d3ba1f29e225697c548a8e0c872aaa
jsoliman <devnull@localhost>
2009-06-23 17:45:12 -0700

    Minor bug fixes to vocabulary section.

---
+++ src/com/preppa/web/pages/contribution/vocab/AddVocab.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.preppa.web.pages.contribution.vocab;
+
+import com.preppa.web.data.DictionaryWordDAO;
+import com.preppa.web.data.VocabDAO;
+import com.preppa.web.entities.DictionaryWord;
+import com.preppa.web.entities.Vocab;
+import com.preppa.web.pages.Index;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.hibernate.annotations.CommitAfter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.corelib.components.Form;
+
+/**
+ *
+ * @author newtonik
+ */
+public class AddVocab {
+    @Property
+    private Vocab word;
+    @Property
+    private Vocab view;
+	@Component(id = "addvocabform")
+	private Form myform;
+    @Property
+    private String dWord;
+    @Inject
+    private DictionaryWordDAO dictionarywordDAO;
+
+    void onValidateForm() {
+    }
+
+    void onActivate(Vocab word) {
+        this.view = word;
+    }
+
+    Object onPassivate() {
+        return view;
+    }
+    @CommitAfter
+    Object onSuccess() {
+        return Index.class;
+    }
+
+    List<String> onProvideCompletionsFromdWord(String partial)
+    {
+        List<DictionaryWord> matches = dictionarywordDAO.findByPartialName(partial);
+
+        List<String> result = new ArrayList<String>();
+
+            for (DictionaryWord a : matches)
+            {
+                result.add(a.getName());
+            }
+            System.out.println(result);
+        return result;
+    }
+}
