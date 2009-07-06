554311056e3fb99d3947977c91fb7e97c3a1972f
jsoliman <devnull@localhost>
2009-07-06 10:14:13 -0700

    more

--- src/com/preppa/web/pages/contribution/vocab/NewVocab.java
+++ src/com/preppa/web/pages/contribution/vocab/NewVocab.java
@@ -6,18 +6,25 @@ package com.preppa.web.pages.contribution.vocab;
 
 import com.preppa.web.data.VocabDAO;
 import com.preppa.web.data.DictionaryWordDAO;
+import com.preppa.web.data.TagDAO;
 import com.preppa.web.entities.ExampleSentence;
 import com.preppa.web.entities.Vocab;
 import com.preppa.web.entities.DictionaryWord;
+import com.preppa.web.entities.Tag;
 import java.sql.Timestamp;
 import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.hibernate.annotations.CommitAfter;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import org.apache.tapestry5.FieldTranslator;
+import org.apache.tapestry5.MarkupWriter;
+import org.apache.tapestry5.ValidationException;
 import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.corelib.components.Form;
+import org.chenillekit.tapestry.core.components.prototype_ui.AutoComplete;
 
 /**
  *
@@ -39,12 +46,18 @@ public class NewVocab {
     private String fDefinition;
     @Property
     private String fSentence;
-    @Property
-    private String fTag;
+    /*@Property
+    private String fTag;*/
 	@Component(id = "vocabform")
 	private Form _form;
     @Inject
     private DictionaryWordDAO dictionarywordDAO;
+    @Component
+    private AutoComplete autoCompleteTag;
+    @Inject
+    private TagDAO tagDAO;
+    @Property
+    private List<Tag> addedTags = new LinkedList<Tag>();
 
     void onValidateForm() {
         List<Vocab> matches = vocabDAO.findByName(fWord);
@@ -52,12 +65,30 @@ public class NewVocab {
         {
             _form.recordError("The word already exists.");
         }
+        addedTags = tagDAO.findAll();
     }
 
+    List<Tag> onProvideCompletionsFromAutocompleteTag(String partial) {
+        List<Tag> matches = tagDAO.findByPartialName(partial);
+        return matches;
+
+    }
 
     /*void onActivate(Vocab word) {
         this.vocab = word;
     }*/
+
+    public boolean getTitleFilled() {
+        return (fWord != null);
+    }
+
+    public boolean getPOSFilled() {
+        return (partofspch != null);
+    }
+
+    public boolean getDefFilled() {
+        return (fDefinition != null);
+    }
 
     void onActivate(int id) {
         DictionaryWord dWord = dictionarywordDAO.findById(id);
@@ -94,7 +125,7 @@ public class NewVocab {
          vocab.setName(fWord);
          vocab.setPartofspeech(partofspch);
          vocab.setDefinition(fDefinition);
-         vocab.setTags(fTag);
+         //vocab.setTags(fTag);
          Timestamp now = new java.sql.Timestamp(System.currentTimeMillis());
          vocab.setCreatedAt(now);
          vocab.setUpdatedAt(now);
@@ -118,4 +149,46 @@ public class NewVocab {
          showvocab.setvocab(vocab);
         return showvocab;
     }
+
+public FieldTranslator getTagTranslator()
+    {
+        return new FieldTranslator<Tag>()
+        {
+            @Override
+          public String toClient(Tag value)
+          {
+                String clientValue = "0";
+                if (value != null)
+                clientValue = String.valueOf(value.getName());
+
+                return clientValue;
+          }
+
+          @Override
+          public void render(MarkupWriter writer) { }
+
+          @Override
+          public Class<Tag> getType() { return Tag.class; }
+
+          @Override
+          public Tag parse(String clientValue) throws ValidationException
+          {
+            Tag serverValue = null;
+//            if(clientValue == null) {
+//                Tag t = new Tag();
+//                t.setName(clientValue);
+//            }
+            System.out.println(clientValue);
+
+            if (clientValue != null && clientValue.length() > 0 && !clientValue.equals("0")) {
+                System.out.println(clientValue);
+                serverValue = tagDAO.findByName(clientValue).get(0);
+            }
+            return serverValue;
+          }
+
+    };
+   }
+
+
 }
