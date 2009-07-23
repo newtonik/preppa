d6f2c422cc11f371e56ae453724f1e39aa3cc577
newtonik <newtonik@gmail.com>
2009-07-23 17:23:24 -0400

    faster query to get total votes

--- src/com/preppa/web/data/VoteDAOHimpl.java
+++ src/com/preppa/web/data/VoteDAOHimpl.java
@@ -1,6 +1,7 @@
 package com.preppa.web.data;
 
 import com.preppa.web.entities.Vote;
+import java.math.BigDecimal;
 import java.util.List;
 import org.chenillekit.hibernate.daos.AbstractHibernateDAO;
 import org.chenillekit.hibernate.utils.SQLString;
@@ -19,19 +20,29 @@ public class VoteDAOHimpl extends AbstractHibernateDAO<Vote, Long> implements Vo
 
     @Override
     public Integer findVoteByContentId(String contentType, Integer contentId) {
-        SQLString sqlString = new SQLString(" FROM Vote v");
+        SQLString sqlString = new SQLString("select sum(v.value) FROM Vote v");
         if(contentType != null && contentId > 0)
         {
             sqlString.addWhereClause("v.contentType  = '" + contentType + "'");
             sqlString.addWhereClause("v.contentId = '" + contentId + "'");
+            sqlString.addGroupField("v.contentType");
+            sqlString.addGroupField("v.contentId");
 
         }
 
-        List<Vote> all = findByQuery(sqlString.toString());
-        Integer sum = 0;
-        for(Vote v : all) {
-            sum += v.getValue();
-        }
+
+        List<Vote> result = findBySQLQuery(sqlString.toString());
+
+        BigDecimal s = (BigDecimal) result.toArray()[0];
+        Integer sum = Integer.valueOf(s.intValue());
+
+        
+//
+        //Integer sum = 0;
+        //Integer sum = result.get(0).getValue();
+//        for(Vote v : all) {
+//            sum += v.getValue();
+//        }
         return sum;
 
     }
