6c492c7eb7fd3de2ff54dca4fc70d95f39d61b7a
jsoliman <devnull@localhost>
2009-08-05 20:22:29 -0700

    Minor fixes.

--- src/com/preppa/web/pages/announcements/ShowAnnouncement.java
+++ src/com/preppa/web/pages/announcements/ShowAnnouncement.java
@@ -6,6 +6,8 @@ package com.preppa.web.pages.announcements;
 
 import com.preppa.web.data.AnnouncementDAO;
 import com.preppa.web.entities.Announcement;
+import com.preppa.web.entities.User;
+import org.apache.tapestry5.annotations.ApplicationState;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
@@ -15,12 +17,18 @@ import org.apache.tapestry5.ioc.annotations.Inject;
  * @author newtonik
  */
 public class ShowAnnouncement {
-
+    @ApplicationState
+    private User user;
     @Persist
     @Property
     private Announcement announce;
     @Inject
     private AnnouncementDAO announceDAO;
+
+
+    public int getUID() {
+        return user.getId();
+    }
 
     Object onActivate(int id) {
         this.announce = announceDAO.findById(id);
