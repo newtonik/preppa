00a437ac661857ecdb6e21d3c796b7b89c706cdc
newtonik <newtonik@gmail.com>
2009-08-05 23:57:54 -0400

    adding history to vocab

---
+++ src/com/preppa/web/pages/contribution/vocab/revisions/VocabRevisionRevisions.java
@@ -0,0 +1,18 @@
+/*
+ * Preppa, Inc.
+ * 
+ * Copyright 2009. All rights reserved.
+ * 
+ * $Id$
+ */
+
+
+package com.preppa.web.pages.contribution.vocab.revisions;
+
+/**
+ *
+ * @author newtonik
+ */
+public class VocabRevisionRevisions {
+
+}
