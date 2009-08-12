a376236f4fc14b94d2eb4e4d665f3a6d02c88542
newtonik <newtonik@gmail.com>
2009-08-11 23:50:10 -0400

    increased the size of gridin pictures and added word count to fck editors.

---
+++ web/js/fck/gridin.js
@@ -0,0 +1,15 @@
+FCKConfig.AutoDetectPasteFromWord = false ;
+FCKConfig.PluginsPath = 'js/fck/plugins/' ;
+FCKConfig.ToolbarSets["Gridin"] = [
+['Source','DocProps','-','Save','NewPage','Preview','-','Templates'],
+['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
+['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+'/',
+['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
+['OrderedList','UnorderedList','-','Outdent','Indent','Blockquote'],
+['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+['Link','Unlink','Anchor'],
+'/',
+['Style','FontFormat','FontName','FontSize'],
+['TextColor']
+] ;
