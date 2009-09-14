f161c50f0df4915e740ca492aeb7b070bfbf9fd3
jsoliman <devnull@localhost>
2009-09-13 23:35:01 -0700

    Missed a file

---
+++ src/com/preppa/web/utils/Constants.java
@@ -0,0 +1,23 @@
+  
+  /*
+   * Preppa, Inc.
+   * 
+   * Copyright 2009. All rights
+  reserved.
+   * 
+   * $Id$
+   */
+
+package com.preppa.web.utils;
+
+/**
+ *
+ * @author Jan Jan
+ */
+public class Constants {
+
+    public static Integer getApprovalThreshhold() {
+        // Number of votes to 
+        return 2;
+    }
+}
