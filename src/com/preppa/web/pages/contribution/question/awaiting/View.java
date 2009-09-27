1cfc626859ea7e8391115d7d1c981997d5194a6b
jsoliman <devnull@localhost>
2009-09-27 15:38:50 -0700

    Change to awaiting approval question.

--- src/com/preppa/web/pages/contribution/question/awaiting/View.java
+++ src/com/preppa/web/pages/contribution/question/awaiting/View.java
@@ -69,7 +69,19 @@ public class View {
         this.qType = questiontype;
         List<Question> allquestions = null;
         isGridin = false;
-        if(questiontype.contains("SentenceCompletion"))
+
+        if(questiontype.contains("Grid-ins")) {
+            System.out.println("Grid-ins");
+            isGridin = true;
+            gridins = gridinDAO.findAll();
+        }
+        else {
+            allquestions = questionDAO.findAllByQuestionType(questiontype);
+        }
+
+
+
+        /*if(questiontype.contains("SentenceCompletion"))
         {
             System.out.println("Sentence Completion");
             allquestions = questionDAO.findAllByQuestionType("Sentence Completion");
@@ -152,7 +164,7 @@ public class View {
         }
         else {
             allquestions = questionDAO.findAllNoRepeat();
-        }
+        }*/
 
 
         if (isGridin == false) {
