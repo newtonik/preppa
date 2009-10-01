fa9281e321a698702fcae54ed332fdce4e9b9d50
jsoliman <devnull@localhost>
2009-10-01 00:24:54 -0700

    Fixes for prompts

--- src/com/preppa/web/pages/contribution/prompt/ViewPrompt.java
+++ src/com/preppa/web/pages/contribution/prompt/ViewPrompt.java
@@ -12,8 +12,11 @@ package com.preppa.web.pages.contribution.prompt;
 
 import com.preppa.web.data.PromptDAO;
 import com.preppa.web.data.TagDAO;
+import com.preppa.web.data.VoteDAO;
 import com.preppa.web.entities.Prompt;
 import com.preppa.web.entities.Tag;
+import com.preppa.web.utils.Constants;
+import com.preppa.web.utils.ContentType;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.tapestry5.annotations.Property;
@@ -36,10 +39,28 @@ public class ViewPrompt {
     private List<Tag> alltags = new ArrayList<Tag>();
     @Property
     private boolean show;
+    @Inject
+    private VoteDAO voteDAO;
     // The String passed in
     @Property
     private String type;
-    
+    @Property
+    private boolean approved;
+    @Property
+    private ContentType contType;
+    //Boolean to decide how far trail of links back goes.
+    @Property
+    private boolean threeLevels;
+
+    public String getApp() {
+        if (approved == true) {
+            return "Approved";
+        }
+        else {
+            return "Awaiting";
+        }
+    }
+
     void onActivate() {
         System.out.println("I'm here too");
         List<Prompt> allprompts = promptDAO.findAll();
@@ -58,27 +79,70 @@ public class ViewPrompt {
     }
 
     Object onActivate(String type) {
+        this.type = type;
+        List<Prompt> allprompts = promptDAO.findAll();
+        contType = ContentType.Prompt;
+        threeLevels = true;
+
+        if (type.contains("Approve") || type.contains("true")) {
+            approved = true;
+            System.out.println("Approve");
+        }
+        else if (type.contains("Awaiting") || type.contains("false")) {
+            approved = false;
+        }
+
         if (type.contains("Respond")) {
             System.out.println("Respond");
             this.type = "Respond to Statement";
-            promptList = promptDAO.findByTopic("Respond to Statement");
+            allprompts = promptDAO.findByTopic("Respond to Statement");
             show = true;
         }
         else if (type.contains("Choosing")) {
             System.out.println("Choose");
             this.type = "Choosing Between Contrasting Statements";
-            promptList = promptDAO.findByTopic("Choosing Between Contrasting Statements");
+            allprompts = promptDAO.findByTopic("Choosing Between Contrasting Statements");
             show = true;
         }
         else if (type.contains("Complete")) {
             System.out.println("Complete");
             this.type = "Complete a Statement";
-            promptList = promptDAO.findByTopic("Complete a Statement");
+            allprompts = promptDAO.findByTopic("Complete a Statement");
             
             show = true;
         }
+        else if (type.contains("Approve")) {
+            System.out.println("Approve");
+            this.type = "Approved";
+            allprompts = promptDAO.findAll();
+            show = true;
+            threeLevels = false;
+        }
+        else if (type.contains("Awaiting")) {
+            System.out.println("Awaiting");
+            this.type = "Awaiting";
+            allprompts = promptDAO.findAll();
+            show = true;
+            threeLevels = false;
+        }
         else {
-            promptList = promptDAO.findAll();
+            allprompts = promptDAO.findAll();
+        }
+
+        this.promptList = new ArrayList<Prompt>();
+        if (approved == true) {
+            for (int i = 0; i < allprompts.size(); i++) {
+                if (voteDAO.findVoteByContentId(contType, allprompts.get(i).getId()) >= Constants.getApprovalThreshhold()) {
+                    promptList.add(allprompts.get(i));
+                }
+            }
+        }
+        else {
+            for (int i = 0; i < allprompts.size(); i++) {
+                if (voteDAO.findVoteByContentId(contType, allprompts.get(i).getId()) < Constants.getApprovalThreshhold()) {
+                    promptList.add(allprompts.get(i));
+                }
+            }
         }
 
         return null;
